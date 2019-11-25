9108438528d92adb216e5f08053e536a6cab9f81
KnightMiner <knightminer4@gmail.com>
2019-11-25 17:20:15 -0600

    Add menu bar with buttons and template

---
+++ _template.php
@@ -0,0 +1,12 @@
+<head>
+  <?php include('include/_imports.php'); ?>
+</head>
+<html>
+	<body>
+  <div class="content">
+    <!-- Place your content here -->
+    
+    <?php include('include/_menu.php'); ?>
+  </div>
+	</body>
+</html>
---
+++ include/_menu.php
@@ -0,0 +1,17 @@
+<div class="bottom-buttons row">
+  <div class="col-4">
+    <a class="btn btn-outline-danger" href="#">
+      <i class="fas fa-scroll"></i>
+    </a>
+  </div>
+  <div class="col-4">
+    <a class="btn btn-outline-success" href="drinks.php">
+      <i class="fas fa-mug-hot"></i>
+    </a>
+  </div>
+  <div class="col-4">
+    <a class="btn btn-outline-primary" href="#">
+      <i class="fas fa-user-friends"></i>
+    </a>
+  </div>
+</div>
