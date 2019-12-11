f21b7959113207bd33b251e569369ef5e0b44c65
InkyMind <Jlester@olivet.edu>
2019-12-11 17:23:54 -0600

    HomePage Additions

--- HomePage.php
+++ HomePage.php
@@ -3,14 +3,15 @@
   <head>
     <?php include('include/_imports.php'); ?>
   </head>
+    
+    
 	<body>
     <?php include('include/_top_menu.php'); ?>
     <div class="content-home">
-      <div style="height:150px;"></div>
-      <div columns: 100px 3; />
       
       <div class="row">
         <div class="col-10">
+          
           <a href="drinks.php"><img src="images/HomePage/Create.png" class="center Create"/></a>
           <a href="#"><img src="images/HomePage/MyDrinks.png" class="center MyDrinks"/></a>
           <a href="Community.php"><img src="images/HomePage/Share.png" class="center Share"/></a>
@@ -18,4 +19,14 @@
       </div>
     </div>
 	</body>
+    <style>
+    { margin: 0; padding: 0; }
+      div {
+        background: url('images/HomePage/Background2.png') no-repeat center center fixed; 
+        -webkit-background-size: contain;
+        -moz-background-size: contain;
+        -o-background-size: contain;
+        background-size: contain;   
+      }
+    </style>
 </html>
