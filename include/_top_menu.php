9b3446b401107ad3524f7807a65b88c1a38d72eb
KnightMiner <knightminer4@gmail.com>
2019-12-10 22:54:57 -0600

    Fix settings link

--- include/_top_menu.php
+++ include/_top_menu.php
@@ -1,6 +1,6 @@
 <div class="top-buttons">
   <div class = "left">
-    <a href="Settings.php"><img src="images/settings.png"/></a>
+    <a href="settings.php"><img src="images/settings.png"/></a>
   </div>
   <div class="right">
     <a href="Themes.php"><img src="images/themes.png"/></a>
