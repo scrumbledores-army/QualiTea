20a142edb6d4933a745a2062e691de0b19ea0cba
InkyMind <Jlester@olivet.edu>
2019-12-11 17:53:46 -0600

    Themes to HomeButton

--- include/_top_menu.php
+++ include/_top_menu.php
@@ -2,7 +2,10 @@
   <div class = "left">
     <a href="settings.php"><img src="images/settings.png"/></a>
   </div>
-  <div class="right">
+  <!--<div class="right">
     <a href="Themes.php"><img src="images/themes.png"/></a>
+  </div>-->
+  <div class="right">
+    <a href="index.php"><img src="images/home_button.png"/></a>
   </div>
 </div>
