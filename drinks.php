3975640802ffe0c951ac96d117ece6c1bfc22e44
KnightMiner <knightminer4@gmail.com>
2019-12-11 18:22:00 -0600

    Add header and footer to drink page

---
+++ drinks.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include('include/_imports.php'); ?>
+  </head>
+  <body>
+    <?php include('include/_top_menu.php'); ?>
+    <div class="content">
+      <div style="padding-top: 150px"></div>
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-3">
+          <input type="image" src="images/coffee.PNG" style="padding: 3px;"/>
+        </div>
+        <div class="col-3">
+          <input type="image" src="images/tea.PNG" style="padding: 3px;"/>
+        </div>
+        <div class="col-4">
+          <a href="drink_maker_smoothie.php"><input type="image" src="images/smoothie.PNG" style="padding: 3px;" /></a>
+        </div>
+      </div>
+    </div>
+    <?php include('include/_bottom_menu.php'); ?>
+  </body>
+<footer>
+
+</footer>
+</html>
